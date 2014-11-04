ca9519339322f644d01e9afc9d94f651260a5e02
Gavin <gavingray1729@gmail.com>
2014-11-04 12:29:26 +0000

    written crude unit test

--- markdownpprint.py
+++ markdownpprint.py
@@ -3,14 +3,19 @@
 import sympy
 import re
 import sys
+import hashlib
 from optparse import OptionParser
 
 
 def main(fname, streaming=False, verbose=False):
     with open(fname) as mdfile:
         md = mdfile.read()
-
+    # regex for finding sympy blocks 
     r = re.compile('<!---sympy(.*)--->')
+    # regex for finding what is on the next line
+    nl = re.compile(r'\n(.*)\n')
+    # regex for finding status information
+    rst = re.compile('<!---status(.*)--->')
     # iterate, finding all equations and adding a pprint section after them
 
     position = 0
@@ -19,6 +24,35 @@ def main(fname, streaming=False, verbose=False):
     while eq is not None:
         # update current position
         position = position + eq.end()
+        # reset flag
+        regenflag = True
+
+        # hash current equation    
+        h = hashlib.md5(eq.encode('utf-8'))
+
+        # check the next line for status information
+        nextline = nl.search(md[position:])
+        status = rst.search(nextline.group())
+
+        if status:
+            statuscontent = status.group().split(' ')
+            statushash = statuscontent[1]
+            # check status against current equation
+            if h.hexdigest() == statushash:
+                regenflag = False
+            # update hash
+            statuscontent[1] = h.hexdigest()
+        else:
+            # create status
+            statuscontent = ["<!---status",h.hexdigest(),"--->"]
+
+        # add statuscontent to md
+        statuscontent = ' '.join(statuscontent)
+        md = md[:position] + '\n' + statuscontent \
+                    + '\n' + md[position:]
+
+        # update position
+        position = position + len(statuscontent) + 4
 
         # extract the sympy expression
         expression = eq.groups()[0]
@@ -27,21 +61,23 @@ def main(fname, streaming=False, verbose=False):
             print('found equation {0} at position {1}'.format(expression,
                                                               position))
 
-        # parse string as sympy expression
-        # might be worth adding a local dict of symbols to
-        # parse_expr but this seems to work fine
-        symeq_obj = sympy.sympify(expression)
+        # regenerate if flag is set:
+        if regenflag:
+            # parse string as sympy expression
+            # might be worth adding a local dict of symbols to
+            # parse_expr but this seems to work fine
+            symeq_obj = sympy.sympify(expression)
 
-        # then we can make the pretty string:
-        prettystring = sympy.pretty(symeq_obj)
+            # then we can make the pretty string:
+            prettystring = sympy.pretty(symeq_obj)
 
-        md = md[:position] + '\n```\n' + prettystring \
-                + '\n```\n' + md[position:]
+            md = md[:position] + '\n```\n' + prettystring \
+                    + '\n```\n' + md[position:]
 
-        if streaming is False:
-            # writing this to the file in the right place
-            with open(fname, 'w') as mdfile:
-                mdfile.write(md)
+            if streaming is False:
+                # writing this to the file in the right place
+                with open(fname, 'w') as mdfile:
+                    mdfile.write(md)
 
         # search again from new position
         eq = r.search(md[position:])
--- test/test.py
+++ test/test.py
@@ -89,5 +89,31 @@ class TestConversion(unittest.TestCase):
         # clean up input file copy
         os.remove(self.test_input_copy)
 
+    def test_status_options(self):
+        '''
+        Tests the status parsing ability
+        '''
+        # if it works, should not produce two copies of equations
+        
+        # copy input file in case it does change
+        shutil.copy(self.test_input, self.test_input_copy)
+        
+        # run main 
+        mdpp.main(self.test_input_copy)
+        # run main again
+        mdpp.main(self.test_input_copy)
+
+        # parse test output (which is input)
+        with open(self.test_input_copy, 'r') as mdpp_out_fh:
+            mdpp_input = mdpp_out_fh.read()
+
+        # check how many equations are in there
+        r = re.compile("```")
+        eqenvs = r.findall(mdpp_input)
+        self.assertEqual(len(eqenvs),4)
+
+        # clean up input file copy
+        os.remove(self.test_input_copy)
+
 if __name__=='__main__':
     unittest.main(module=__name__, buffer=True, exit=False)
